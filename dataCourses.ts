186fe8d5313669ff45390d8003f69ce95750edb0
Nicolas Orjuela <n.orjuela@uniandes.edu.co>
2021-04-13 16:28:12 -0500

    Taller Typescript 1

---
+++ dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en equipo", "Cesar Forero", 3),
+  new Course("Sistemas Transaccionales", "Claudia Jimenez", 3),
+  new Course("Diseño de Algoritmos", "Rodrigo Cardoso", 3),
+  new Course("TI en las Organizaciones", "Ricardo Gómez", 3),
+  new Course("Química", "Mario Macias", 3)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW en equipo", "Cesar Forero", 3),
+    new Course("Sistemas Transaccionales", "Claudia Jimenez", 3),
+    new Course("Diseño de Algoritmos", "Rodrigo Cardoso", 3),
+    new Course("TI en las Organizaciones", "Ricardo Gómez", 3),
+    new Course("Química", "Mario Macias", 3)
+];
